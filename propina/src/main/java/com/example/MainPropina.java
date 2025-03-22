be255300882392b486dbfa60a29e87f513b9dff3
Mayra Alzate <nadja.minni@gmail.com>
2025-03-21 21:18:37 -0500

    Renombrar clase Main y corrección de print de consola para valorCuenta

--- propina/src/main/java/com/example/MainPropina.java
+++ propina/src/main/java/com/example/MainPropina.java
@@ -2,7 +2,7 @@ package com.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class MainPropina {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -16,7 +16,7 @@ public class Main {
         double valorTotal = (valorCuenta+valorPropina);
         
 
-        System.out.printf("Valor (e la cuenta es: $%.2f%n" ,valorCuenta  );
+        System.out.printf("El valor de la cuenta es: $%.2f%n" ,valorCuenta  );
         System.out.printf("El porcentaje de propina es: %.1f%%%n" ,porcentajePropina  );
         System.out.printf("El valor de la propina es: $%.2f%n" ,valorPropina  );
         System.out.printf("El valor total a pagar es: $%.2f%n" , valorTotal  );
